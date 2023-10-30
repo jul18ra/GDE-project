9360f92118dd36381564fdbb04acb60c317bbf45
jul18ra <145108891+jul18ra@users.noreply.github.com>
2023-10-30 11:58:44 +0200

    Added enemies
    Enemies follow player

---
+++ GDE_Warpshot_test/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float speed = 50f;
+    private Vector2 direction;
+    private Rigidbody2D enemyRb;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Enemies move towards player
+        direction = player.transform.position - transform.position;
+        enemyRb.MovePosition((Vector2)transform.position + (speed * Time.deltaTime * direction.normalized));
+    }
+}
