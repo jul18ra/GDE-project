eab969ce5322f0c55d86d2be5d549946802315c9
jul18ra <145108891+jul18ra@users.noreply.github.com>
2023-12-11 16:01:17 +0200

    Multiple additions
    - Main menu animations
    - Set up menu script
    - Control scheme panel

---
+++ GDE_Warpshot_test/Assets/Scripts/Scene/MainMenuScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    private int mainMenu = 0;
+    private int levelScene = 1;
+    private int gameOver = 2;
+    
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(levelScene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OpenControls()
+    {
+
+    }
+}
